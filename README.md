25d3bd4a65c1740d2cc0b038d6e47535a3bdf81b
Ben Adida <ben@adida.net>
2023-10-02 18:58:30 -0400

    adding shuffling

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ This is a client of the electionguard library for RAVE purposes.
 
 This is for Linux.
 
+- Get Java 17
+
+```
+sudo apt install openjdk-17-jre
+```
+
 - Get SDKMan! https://sdkman.io/
 
 - Install kotlin
@@ -50,18 +56,10 @@ mkdir <WORKSPACE_DIR>
 
 This will output the encrypted ballot, which should be written to a file.
 
-## Proof of Concept of Mixnet
-
-```
-./scripts/shuffle-proof-of-concept.sh <WORKSPACE_DIR> <directory_of_encrypted_ballots>
-```
-
-This will extract the first ciphertext from each encrypted ballot and pass them through a verificatum mixnet configured with the right public key.
-
 ## Homomorphically Tally Ballots
 
 ```
-./scripts/tabulate-encrypted-ballots.sh <WORKSPACE_DIR> <directory_of_encrypted_ballots>
+./scripts/tabulate-encrypted-ballots.sh <WORKSPACE_DIR>
 ```
 
 This will return the tally JSON, which should be written to a file
@@ -69,102 +67,20 @@ This will return the tally JSON, which should be written to a file
 ## Decrypt the Tally
 
 ```
-./scripts/decrypt-encrypted-tally.sh <WORKSPACE_DIR> <encrypted_tally_file>
+./scripts/decrypt-tally.sh <WORKSPACE_DIR>
 ```
 
-## 
-
-# Old stuff
-
-- Install ElectionGuard via
-
-https://github.com/JohnLCaron/egk-webapps
-
-set an environment variable for what follows
+## Shuffle the Ciphertexts
 
 ```
-export EG_HOME=<your egk-webapps path>
+./scripts/shuffle-ciphertexts.sh <WORKSPACE_DIR>
 ```
 
-
-## Prepare ElectionGuard Election Manifest and All
-
-Convert the VotingWorks format -- this is basically the ballot definition
-
-```
-mkdir testOut
-node ./convert-vx-to-eg.js ./famous-names-election.json testOut/manifest.json
-```
-
-Create the full EG Election Configuration
-
-```
-java \
-  -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 \
-  -classpath ${EG_HOME}/egklib/build/libs/egklib-all.jar \
-  electionguard.cli.RunCreateElectionConfig \
-    -manifest testOut/manifest.json \
-    -nguardians 3 \
-    -quorum 3 \
-    -out testOut/cliWorkflow/config \
-    --baux0 device42 \
-    --chainCodes
-```
+This extracts only the ciphertexts from ElectionGuard encrypted ballots (no proofs), and passes them through the Verificatum Mixnet.
 
 
-## Generate Trustee Keypairs and Election KeyPairs (aka Key Ceremony)
+## Plaintext Equivalence Proof
 
-```
-java \
-  -classpath ${EG_HOME}/egklib/build/libs/egklib-all.jar \
-  electionguard.cli.RunTrustedKeyCeremony \
-    -in testOut/cliWorkflow/config \
-    -trustees testOut/cliWorkflow/keyceremony/trustees \
-    -out testOut/cliWorkflow/keyceremony 
-```
+coming soon.
 
-## Encrypt a Ballot
-
-We have to use the initialized configuration now, coming out of the key ceremony.
-
-Start the server
-
-```
-java \
-  -classpath ${EG_HOME}/encryptserver/build/libs/encryptserver-all.jar \
-  electionguard.webapps.server.RunEgkServerKt \
-  --inputDir testOut/cliWorkflow/keyceremony \
-  --outputDir testOut/encrypt/RunEgkServer
-```
-
-Encrypt a ballot against the server
-
-```
-java \
-  -classpath ${EG_HOME}/encryptclient/build/libs/encryptclient-all.jar \
-  electionguard.webapps.client.RunEgkClientKt \
-  --inputDir testOut/cliWorkflow/keyceremony \
-  --outputDir testOut/encrypt/RunEgkServer
-```
-
-## Accumulate the Tally
-
-```
-java \
-  -classpath ${EG_HOME}/egklib/build/libs/egklib-all.jar \
-  electionguard.cli.RunAccumulateTally \
-    -in testOut/encrypt/RunEgkServer \
-    -out testOut/encrypt/RunEgkServer 
-```
-
-## Run Tally Decryption
-
-```
-java \
-  -classpath ${EG_HOME}/egklib/build/libs/egklib-all.jar \
-  electionguard.cli.RunTrustedTallyDecryption \
-    -in testOut/encrypt/RunEgkServer \
-    -trustees testOut/cliWorkflow/keyceremony/trustees \
-    -out testOut/encrypt/RunEgkServer 
-```
 
---
+++ scripts/shuffle-ciphertexts.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+WORKSPACE_DIR=$1
+
+if [ -z "${WORKSPACE_DIR}" ]; then
+    exit 1
+fi
+
+EG_WORKSPACE="${WORKSPACE_DIR}/eg/encryption"
+CONSTANTS="${EG_WORKSPACE}/constants.json"
+ELECTION_PARAMS="${EG_WORKSPACE}/election_initialized.json"
+
+VERIFICATUM_WORKSPACE="${WORKSPACE_DIR}/vf"
+rm -rf ${VERIFICATUM_WORKSPACE}/*
+mkdir -p ${VERIFICATUM_WORKSPACE}
+
+# extract p g g
+P=`cat ${CONSTANTS} | jq -r '.large_prime' | tr '[:upper:]' '[:lower:]'`
+Q=`cat ${CONSTANTS} | jq -r '.small_prime' | tr '[:upper:]' '[:lower:]'`
+G=`cat ${CONSTANTS} | jq -r '.generator' | tr '[:upper:]' '[:lower:]'`
+
+# generate group description for Verificatum
+GROUP=$(vog -gen ModPGroup -roenc -explic ${P} ${G} ${Q} | sed "s/[^:]*:://g")
+
+# convert it to JSON
+echo "${GROUP}" | sed "s/[^:]*:://g" > ./_tmp_group_description
+GROUP_JSON=`vbt -hex ./_tmp_group_description`
+rm ./_tmp_group_description
+
+# generate verificatum configuration
+MIXER_NAME="MergeMixer"
+
+vmni -prot -sid "FOO" -name ${MIXER_NAME} -nopart 1 -thres 1 \
+     -pgroup "${GROUP}" -keywidth "1" ${VERIFICATUM_WORKSPACE}/localProtInfo.xml
+
+# generate mixer info, including private key for signing
+vmni -party -name "${MIXER_NAME}" \
+     -http http://localhost:8041 \
+     -hint localhost:4041 \
+     ${VERIFICATUM_WORKSPACE}/localProtInfo.xml ${VERIFICATUM_WORKSPACE}/privInfo.xml ${VERIFICATUM_WORKSPACE}/protInfo.xml
+
+# extract public key from ElectionGuard
+Y=`cat ${ELECTION_PARAMS} | jq -r '.joint_public_key' | tr '[:upper:]' '[:lower:]'`
+
+# convert pk to Verificatum JSON
+echo ${GROUP_JSON} | jq --arg g "00$G" --arg y "00$Y" '[., [$g, $y]]' > ${VERIFICATUM_WORKSPACE}/publickey.json
+
+# convert to Verificatum RAW
+vmnc -e -pkey ${VERIFICATUM_WORKSPACE}/protInfo.xml -ini seqjson -outi raw ${VERIFICATUM_WORKSPACE}/publickey.json ${VERIFICATUM_WORKSPACE}/publickey.raw
+
+# import it
+vmn -setpk ${VERIFICATUM_WORKSPACE}/privInfo.xml ${VERIFICATUM_WORKSPACE}/protInfo.xml ${VERIFICATUM_WORKSPACE}/publickey.raw
+
+# extract ciphertext components to V JSON import format
+echo "[" > ${VERIFICATUM_WORKSPACE}/input-ciphertexts.json
+LOOP_NUM=0
+
+for file in ${EG_WORKSPACE}/encrypted_ballots/*/eballot-*.json; do
+    # Check if file exists (this avoids the loop body executing if no files match the pattern)
+    if [[ -f "$file" ]]; then
+        echo "Loading ballot: $file"
+
+	# pull all the ciphertexts for one ballot into a multi-ciphertext wide input for Verificatum
+	NEW_BALLOT=`cat $file | jq '[.contests[] | .selections[] | .encrypted_vote]' | jq '[[.[] | .pad | "00" + .], [.[] | .data | "00" + .]]' | tr '[:upper:]' '[:lower:]'`
+
+	# compute the width (yes this will happen every time and overwrite the previous value, that's fine)
+	WIDTH=`echo "$NEW_BALLOT" | jq '.[0] | length'`
+
+	# append ballot to the array of ballots
+	echo "appending ballot"
+	if [ $LOOP_NUM -gt 0 ]; then
+	    echo "," >> ${VERIFICATUM_WORKSPACE}/input-ciphertexts.json
+	fi
+	echo "$NEW_BALLOT" >> ${VERIFICATUM_WORKSPACE}/input-ciphertexts.json
+	echo "done appending ballot"
+    fi
+    LOOP_NUM=1
+done
+
+echo "]" >> ${VERIFICATUM_WORKSPACE}/input-ciphertexts.json
+
+# convert ciphertexts to V raw format
+vmnc -e -ciphs -width "${WIDTH}"  -ini seqjson -outi raw \
+     ${VERIFICATUM_WORKSPACE}/protInfo.xml ${VERIFICATUM_WORKSPACE}/input-ciphertexts.json ${VERIFICATUM_WORKSPACE}/input-ciphertexts.raw
+
+
+AUXSID=`date "+%s" | sed "s/ /_/g"`
+
+# shuffle once
+vmn -shuffle -width "${WIDTH}" -auxsid "${AUXSID}" \
+    ${VERIFICATUM_WORKSPACE}/privInfo.xml \
+    ${VERIFICATUM_WORKSPACE}/protInfo.xml \
+    ${VERIFICATUM_WORKSPACE}/input-ciphertexts.raw ${VERIFICATUM_WORKSPACE}/after-mix-1-ciphertexts.raw
+
+AUXSID=`date "+%s" | sed "s/ /_/g"`
+
+# shuffle twice
+vmn -shuffle -width "${WIDTH}" -auxsid "${AUXSID}" \
+    ${VERIFICATUM_WORKSPACE}/privInfo.xml \
+    ${VERIFICATUM_WORKSPACE}/protInfo.xml \
+    ${VERIFICATUM_WORKSPACE}/after-mix-1-ciphertexts.raw ${VERIFICATUM_WORKSPACE}/after-mix-2-ciphertexts.raw
+
+# convert output ciphertexts to JSON format
+vmnc -ciphs -width "${WIDTH}" -ini raw -outi seqjson \
+     ${VERIFICATUM_WORKSPACE}/protInfo.xml ${VERIFICATUM_WORKSPACE}/after-mix-2-ciphertexts.raw ${VERIFICATUM_WORKSPACE}/after-mix-2-ciphertexts.json
+
+
