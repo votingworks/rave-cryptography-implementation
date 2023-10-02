28d94bc3a5818b962cd3b1a47cc850042e828573
Ben Adida <ben@adida.net>
2023-10-02 19:17:12 -0400

    jdk and node

--- scripts/install-basic.sh
+++ scripts/install-basic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install jq openjdk-17-jre curl zip
+sudo apt install jq openjdk-17-jdk curl zip nodejs
 curl -s "https://get.sdkman.io" | bash
 source ~/.sdkman/bin/sdkman-init.sh
 sdk install kotlin
